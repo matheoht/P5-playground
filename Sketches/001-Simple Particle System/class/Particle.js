1ab96413ff13605545e17dcc52aa75dcffe1049b
matheoht <108397640+matheoht@users.noreply.github.com>
2022-09-12 20:58:07 +0200

    refactor: Particle class of "001-Simple Particle System"

--- Sketches/001-Simple Particle System/class/Particle.js
+++ Sketches/001-Simple Particle System/class/Particle.js
@@ -1,22 +1,22 @@
 class Particle {
   constructor() {
-    this.location = createVector(350, 550);
-    this.velocity = createVector(random(-1.5, 1.5), random(-5, -1));
+    this.location = createVector(350, 650);
+    this.velocity = createVector(random(-2, 2), random(-6, -1));
     this.size = PARTICLE_SIZE;
   }
 
   update() {
     //If the size of the particle is below 0 reset the particle otherwise update and display the particle
     if (this.size < 0) {
-      this.location = createVector(350, 550);
-      this.velocity = createVector(random(-1, 1), random(-5, -1));
+      this.location = createVector(350, 650);
+      this.velocity = createVector(random(-2, 2), random(-6, -1));
       this.size = PARTICLE_SIZE;
     } else {
       this.size -= PARTICLE_LIFETIME;
       this.location.add(this.velocity);
 
       //Display particle
-      stroke(255 - this.size * PARTICLE_BRIGHTNESS);
+      stroke(255 - this.size * 10,255 - this.size * PARTICLE_BRIGHTNESS,0);
       fill(255, 0);
       ellipse(this.location.x, this.location.y, this.size);
     }
